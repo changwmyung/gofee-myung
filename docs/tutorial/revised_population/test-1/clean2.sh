516ec9b36df9d3113f8e2dfbc57048b2374fd2ba
gbsim <rlaqja098@gmail.com>
2023-04-13 14:51:05 +0900

    Application of modified methods for population updates

---
+++ docs/tutorial/revised_population/test-1/clean2.sh
@@ -0,0 +1,1 @@
+rm -r gofee.* restart.pickl search.log structures.traj vasp bfgs* POSCAR-sample unrelaxed_candidates.traj relaxed_candidates.traj au50sto*
--- docs/tutorial/revised_population/test_new_version_5/clean2.sh
+++
@@ -1,1 +0,0 @@
-rm -r gofee.* restart.pickl search.log structures.traj vasp bfgs* POSCAR-sample unrelaxed_candidates.traj relaxed_candidates.traj au50sto*
